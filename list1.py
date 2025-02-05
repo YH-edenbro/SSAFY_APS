440c3116a17196630020f393a13c34ced80c163c
YH-edenbro <hyun96227@gmail.com>
2025-02-05 17:53:06 +0900

    3문제

--- list1.py
+++ list1.py
@@ -46,4 +46,23 @@ for test_case in range(1, T+1):
     print(f'#{test_case} {big_n - small_n}')
 
 
+# View
+
+for test_case in range(1, 11):
+    n = int(input())
+    height = list(map(int, input().split()))
+
+    window = 0
+
+    for i in range(2, n-2):
+        if height[i] > height[i-2] and height[i] > height[i+2]:
+            if height[i] > height[i+1] and height[i] > height[i-1]:
+                window = window + height[i] - max(height[i+1], height[i+2], height[i-1], height[i-2])
+
+    print(f'#{test_case} {window}')
+
+
+
+
+
 

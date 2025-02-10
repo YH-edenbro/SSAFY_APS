559682eddafeb3ba79f0a09d852e873476a43c59
YH-edenbro <hyun96227@gmail.com>
2025-02-10 17:54:59 +0900

    사다리전까지

---
+++ List2_2.py
@@ -0,0 +1,134 @@
+# 4839 이진탐색
+
+def binary_search_faster(total_page, page_a, page_b):
+    book = [x for x in range(P+1)]
+
+    # page_a 이진탐색으로 찾을 때 걸리는 시간
+    start = 1
+    end = P
+    a = 0
+    while start <= end:
+        middle = (start + end) // 2
+        if book[middle] == Pa:
+            a += 1
+            break
+        elif book[middle] > Pa:
+            end = middle
+            a += 1
+        else:
+            start = middle
+            a += 1
+
+    # pag_b를 이진탐색으로 찾을 때 걸리는 시간
+    start = 1
+    end = P
+    b = 0
+    while start <= end:
+        middle = (start + end) // 2
+        if book[middle] == Pb:
+            b += 1
+            break
+        elif book[middle] > Pb:
+            end = middle
+            b += 1
+        else:
+            start = middle
+            b += 1
+
+    if a > b:
+        return 'B'
+    elif a < b:
+        return 'A'
+    else:
+        return 0
+
+
+T = int(input())
+
+for tc in range(1, T+1):
+    P, Pa, Pb = map(int, input().split())
+
+    print(f"#{tc} {binary_search_faster(P, Pa, Pb)}")
+
+
+# 특별한 정렬
+
+def special_sort(a, n):
+    for i in range(n):
+        special_index = i
+        for j in range(i+1, n):
+            if i % 2 == 0:
+                if a[special_index] < a[j]:
+                    special_index = j
+            else:
+                if a[special_index] > a[j]:
+                    special_index = j
+        a[i], a[special_index] = a[special_index], a[i]
+
+
+T = int(input())
+
+for tc in range(1, T+1):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    special_sort(arr, n)
+
+    print(f"#{tc}", *(arr[:10]))
+
+# 숫자 배열 회전
+
+# 90도 회전
+def rotate_90(a):
+    return list(map(list, zip(*a[::-1])))
+
+
+# 180도 회전
+def rotate_180(a):
+    a = list(map(list, zip(*a[::-1])))
+    return list(map(list, zip(*a[::-1])))
+
+
+# 270도 회전(90도 반시계 회전)
+def rotate_270(a):
+    return list(zip(*a))[::-1]
+
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [list(map(str, input().split())) for _ in range(N)]
+
+    arr_90 = rotate_90(arr)
+    arr_180 = rotate_180(arr)
+    arr_270 = rotate_270(arr)
+
+    print(f"#{tc}")
+    for i in range(N):
+        b = arr_90[i][:]
+        c = arr_180[i][:]
+        d = arr_270[i][:]
+        print(f'{"".join(b)} {"".join(c)} {"".join(d)}')
+
+
+# 풍선팡 1
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    balloon = [list(map(int, input().split())) for _ in range(N)]  # 꽃가루가 들어있는 풍선 배열
+
+    max_flower = 0  # 최대 터지는 꽃가루
+    for i in range(N):
+        for j in range(M):
+            flower = balloon[i][j]  # 현재 위치에 있는 풍선의 꽃가루가 몇개인지
+            s = balloon[i][j]  # 현재 꽃까루까지 포함해야 하기 때문에
+            for di, dj in [[0, 1], [1, 0], [0, -1], [-1, 0]]:  # 상하좌우 살펴보기
+                for c in range(1, flower+1):  # 풍선의 꽃가루 개수만큼 주변을 탐색
+                    ni, nj = i + di * c, j + dj * c
+                    if 0 <= ni < N and 0 <= nj < M:
+                        s += balloon[ni][nj]
+            max_flower = max(max_flower, s)
+
+    print(f"#{tc} {max_flower}")

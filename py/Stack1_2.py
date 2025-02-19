1f6b0bcbdca5b2e2e6095bc565e79642ed103442
YH-edenbro <hyun96227@gmail.com>
2025-02-19 23:04:59 +0900

    graph

--- py/Stack1_2.py
+++ py/Stack1_2.py
@@ -70,3 +70,39 @@ for _ in range(1,11):
 
 
     print(f"#{tc} {dfs(0, 99)}")
+
+# 그래프 경로
+
+def dfs(s, g):
+    stack = []
+    visited = [0] * (V+1)
+
+    while True:
+        if s == g:
+            return 1
+        if visited[s] == 0:
+            visited[s] = 1
+        for w in graph[s]:
+            if visited[w] == 0:
+                stack.append(s)
+                s = w
+                break
+        else:
+            if stack:
+                s = stack.pop()
+            else:
+                break
+    return 0
+
+T = int(input())
+for tc in range(1, T+1):
+    V, E = map(int, input().split())
+    graph = [[] for _ in range(V+1)]
+
+    for _ in range(E):
+        n1, n2 = map(int, input().split())
+        graph[n1].append(n2)
+
+    S, G = map(int, input().split())
+
+    print(f"#{tc} {dfs(S, G)}")

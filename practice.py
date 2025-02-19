987e122fd486c690f46956eacfaf55e91bcc1eb3
YH-edenbro <hyun96227@gmail.com>
2025-02-19 17:56:58 +0900

    0219

--- practice.py
+++ practice.py
@@ -1,88 +1,40 @@
-# 오셀로 미완성
+# SWEA 그래프 경로
 
-T = int(input())
-for tc in range(1, T+1):
-    N, M = map(int, input().split())
+import sys
+sys.stdin = open("4871_input.txt", "r")
 
-    # 빈 보드 만들기
-    board = [[0] * N for _ in range(N)]
 
-    # 기본 세팅하기 (흑돌의 색 : 1, 백돌의 색 : 2)
-    center = N//2  # 보드의 중앙 인덱스
-    board[center][center - 1] = board[center - 1][center] = 1  # 흑돌
-    board[center - 1][center - 1] = board[center][center] = 2  # 백돌
+def dfs(s, g):
+    stack = []
+    visited = [0] * (V+1)
 
-    # 검은색 돌과 흰색 돌 기본으로 2개씩
-    black_stone = 2
-    white_stone = 2
-    # 게임 입력 받기
-    for _ in range(M):
-        i, j, stone = map(int, input().split())
-        # 주어진 i, j 기준으로 검사하기
-        # i, j가 인덱스가 아니어서 인덱스형태로 만들기
-        i -= 1
-        j -= 1
-        # 검은돌을 뒀을 때
-        if stone == 1:
-            board[i][j] = 1
-            black_stone += 1
-            for row, col in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
-                for k in range(1, N-1):
-                    ni, nj = i+row*k, j+col*k
-                    if 0 < ni < N and 0 < nj < N:
-                        if board[ni][nj] == 1:
-                            black_stone += 1
-                            white_stone -= 1
-
-        # 흰돌을 뒀을 때
+    while True:
+        if s == g:
+            return 1
+        if visited[s] == 0:
+            visited[s] = 1
+        for w in graph[s]:
+            if visited[w] == 0:
+                stack.append(s)
+                s = w
+                break
         else:
-            board[i][j] = 2
-            white_stone += 1
-            for row, col in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
-                for k in range(1, N-1):
-                    ni, nj = i+row*k, j+col*k
-                    if 0 < ni < N and 0 < nj < N:
-                        if board[ni][nj] == 2:
-                            white_stone += 1
-                            black_stone -= 1
-
-
-    print(f"#{tc} {black_stone} {white_stone}")
-    print(board)
-
-
-# 오목 미완성
+            if stack:
+                s = stack.pop()
+            else:
+                break
+    return 0
 
 T = int(input())
 for tc in range(1, T+1):
-    N = int(input())
+    V, E = map(int, input().split())
+    graph = [[] for _ in range(V+1)]
 
-    omok = []
-    # 오목 게임 진행
-    for _ in range(N):
-        stone = list(input())
-        omok.append(stone)
+    for _ in range(E):
+        n1, n2 = map(int, input().split())
+        graph[n1].append(n2)
+        graph[n2].append(n1)
 
-    ans = 'NO'
+    S, G = map(int, input().split())
 
-    for i in range(N):
-        for j in range(N):
-            if omok[i][j] == 'o':
-                for di, dj in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
-                    cnt = 1
-                    if ans == 'YES':
-                        break
-                    for k in range(N-1):
-                        ni, nj = i + di*k, j + dj*k
-                        if 0 <= ni < N and 0 <= nj < N:
-                            if omok[ni][nj] == '.':
-                                break  # for k
-                            elif omok[ni][nj] == 'o':
-                                cnt += 1
-                                if cnt == 5:
-                                    ans = 'YES'
-                                    break  # for k
-
-
-    print(f"#{tc} {ans}")
-
+    print(f"#{tc} {dfs(S, G)}")

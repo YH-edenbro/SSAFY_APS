455d2960f2cc7e040e6838ffad5901858283e34d
YH-edenbro <hyun96227@gmail.com>
2025-02-28 17:56:11 +0900

    cook

--- practice.py
+++ practice.py
@@ -1,42 +1,38 @@
-# 등산로 조성
-
 import sys
-sys.stdin = open("", "r")
+sys.stdin = open("sample_input (1).txt", "r")
 
+
+def dfs(sy_i, sy_j, a):
+    global min_differ
+
+    if sy_i == N-1 or sy_j == N-1:
+        return
+
+    if sy_i != sy_j and not visited[sy_i][sy_j] and not visited[sy_j][sy_i]:
+        b = synergy[sy_i][sy_j] + synergy[sy_j][sy_i]
+        visited[sy_i][sy_j] = 1
+        visited[sy_j][sy_i] = 1
+        min_differ = min(min_differ, abs(a-b))
+
+    dfs(sy_i + 1, sy_j, a)
+
+    dfs(sy_i, sy_j + 1, a)
 
 
 T = int(input())
 
 for tc in range(1, T+1):
-    N, K = map(int, input().split())
-    map_info = [list(map(int, input().split())) for _ in range(N)]
+    N = int(input())  # N: 식재료의 수
+    synergy = [list(map(int, input().split())) for _ in range(N)]
+    visited = [[0] * N for _ in range(N)]
 
+    min_differ = 20000  # 시너지의 최대 정수
 
-    def dfs(before_idx, k):
-        dx = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-        k_cnt = 0
-        road_cnt = 0
-        after_idx = []
-
-        if map_info[after_idx[0]][after_idx[1]] < map_info[before_idx[0]][before_idx[1]]:
-            return road_cnt
-
-    # 2차원 배열에서 최대 값 찾기
-    def find_max(arr):
-        max_num = 0
-        for i in range(N):
-            for j in range(N):
-                if arr[i][j] > max_num:
-                    max_num = arr[i][j]
-        return max_num
-
-    top = find_max(map_info)
-
-    long_road = 0
     for i in range(N):
         for j in range(N):
-            if map_info[i][j] == top:
-                start = [i, j]
+            if i == j:
+                continue
+            A_taste = synergy[i][j] + synergy[j][i]
+            dfs(0, 0, A_taste)
 
-    print(f"{tc}")
+    print(f"{tc} {min_differ}")
